dcc437d7cb3380b2473de756eb4b66ed3c919a5c
Warren R Bank <warren.r.bank@gmail.com>
2022-10-27 06:38:39 -0700

    consolidate the sending of each playlist to a single API POST
    requires: ExoAirPlayer v2.0.49 or higher

--- tests/1_unit/02_lib_networking_cast_video/index.js
+++ tests/1_unit/02_lib_networking_cast_video/index.js
@@ -5,18 +5,24 @@ const configs = {
   port: 8192
 }
 
-const run_test = async function(video_url, queue) {
-  const data = await cast_video(configs.host, configs.port, video_url, queue)
+const run_test = async function(video_urls, queue) {
+  const data = await cast_video(configs.host, configs.port, video_urls, queue)
 
   const statusCode = data.response.statusCode
 
-  console.log(JSON.stringify({video_url, statusCode}, null, 4))
+  console.log(JSON.stringify({video_urls, statusCode}, null, 4))
 }
 
 const run_all_tests = async function() {
-  await run_test('rtmp://104.194.11.25:80/live/uVMS_ef7082d0422d07265f6c',     false)
-  await delay(5000)
-  await run_test('https://www.cbsnews.com/common/video/cbsn_header_prod.m3u8', true)
+  await run_test('rtmp://104.194.11.25:80/live/uVMS_ef7082d0422d07265f6c', false)
+  await delay(10000)
+  await run_test(
+    [
+      'https://www.cbsnews.com/common/video/cbsn_header_prod.m3u8',
+      'https://www.cbsnews.com/common/video/cbsn-ny-prod.m3u8'
+    ],
+    true
+  )
 }
 
 run_all_tests()
